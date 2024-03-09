b394fe542a957707fef81b00fbeb8c0d3eff4e33
fromNullTo1 <111220707+fromNullTo1@users.noreply.github.com>
2024-03-09 16:44:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,65 @@
+// function updateDateTime() {
+//     const now = new Date();
+//     const hours24 = now.getHours();
+//     const minutes = now.getMinutes();
+//     let hours = hours24 % 12 || 12;
+//     let timeString = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+
+//     document.getElementById('time').textContent = timeString;
+
+//     let daytime;
+//     if (hours24 < 6) {
+//         daytime = 'ночь';
+//     } else if (hours24 < 12) {
+//         daytime = 'утро';
+//     } else if (hours24 < 18) {
+//         daytime = 'день';
+//     } else {
+//         daytime = 'вечер';
+//     }
+
+//     document.getElementById('daytime').textContent = daytime;
+
+//     const daysOfWeek = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+//     const dayOfWeek = daysOfWeek[now.getDay()];
+//     document.getElementById('dayOfWeek').textContent = `${dayOfWeek}`;
+// }
+
+// function adjustColors() {
+//     const hours24 = new Date().getHours();
+    
+//     if (hours24 >= 20 || hours24 < 5) {
+//         document.body.classList.add("dark-mode");
+//     } else {
+//         document.body.classList.remove("dark-mode");
+//     }
+    
+//     console.log('test');
+// }
+
+// function toggleMode() {
+//     document.body.classList.toggle("dark-mode");
+// }
+
+// document.body.addEventListener('click', () => {
+//     toggleMode();
+// });
+
+// setInterval(updateDateTime, 1000);
+// setInterval(adjustColors, 300000);
+// setInterval(function() {
+//     window.location.reload();
+// }, 600000);
+
+// updateDateTime();
+
+document.addEventListener('DOMContentLoaded', function() {
 function updateDateTime() {
     const now = new Date();
     const hours24 = now.getHours();
     const minutes = now.getMinutes();
     let hours = hours24 % 12 || 12;
     let timeString = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-    //hours < 10 ? '0' + hours : hours
 
     document.getElementById('time').textContent = timeString;
 
@@ -23,7 +78,10 @@ function updateDateTime() {
 
     const daysOfWeek = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
     const dayOfWeek = daysOfWeek[now.getDay()];
+    
     document.getElementById('dayOfWeek').textContent = `${dayOfWeek}`;
+    
+    console.log('test1');
 }
 
 function adjustColors() {
@@ -35,7 +93,7 @@ function adjustColors() {
         document.body.classList.remove("dark-mode");
     }
     
-    console.log('test');
+    console.log('test2');
 }
 
 function toggleMode() {
@@ -47,9 +105,10 @@ document.body.addEventListener('click', () => {
 });
 
 setInterval(updateDateTime, 1000);
-setInterval(adjustColors, 300000);
+setInterval(adjustColors, 1000);
 setInterval(function() {
     window.location.reload();
 }, 600000);
 
 updateDateTime();
+});
